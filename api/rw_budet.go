7b2a5e446309b52ecc8d35dbad729ebd56aa15ff
JSellers0 <JSellers0@pm.me>
2025-12-20 11:36:35 -0500

    init: basic api route

--- api/rw_budet.go
+++ api/rw_budet.go
@@ -3,11 +3,23 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	da "rw_budget/api/api/internal"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
+
+type account struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+}
+
+var accounts = []account{
+	{ID: 1, Name: "Test", Type: "Credit Card"},
+}
 
 func main() {
 	err := godotenv.Load()
@@ -22,4 +34,13 @@ func main() {
 		log.Fatal(pingErr)
 	}
 	fmt.Println("Connected!")
+
+	router := gin.Default()
+	router.GET("/accounts", getAccounts)
+
+	router.Run("localhost:8080")
+}
+
+func getAccounts(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, accounts)
 }
