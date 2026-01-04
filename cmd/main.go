3eeb1acaa73f3845f18742a0724cd21d682a09f7
JSellers0@maestro <rw-admin.unruly589@passinbox.com>
2026-01-04 07:24:28 -0500

    split routes from main

--- cmd/main.go
+++ cmd/main.go
@@ -2,47 +2,22 @@ package main
 
 import (
 	"net/http"
-	h "rw_budget/api/handlers"
-	s "rw_budget/api/services"
 	"time"
 
 	"github.com/gin-gonic/gin"
+
+	router "rw_budget/api/routes"
 )
 
 func main() {
-	ah := h.NewAccountHandler(s.NewAccountService())
-	ch := h.NewCategoryHandler(s.NewCategoryService())
-	cfh := h.NewCashflowHandler(s.NewCashflowService())
-	th := h.NewTransactionHandler(s.NewTransactionService())
-
-	router := gin.Default()
-	router.GET("/health", func(c *gin.Context) {
+	svr := gin.Default()
+	svr.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"status":    "available",
 			"timestamp": time.Now().Unix(),
 		})
 	})
-	router.GET("/accounts", ah.GetAccounts)
-	router.POST("/accounts", ah.PostAccount)
-	router.GET("/accounts/:id", ah.GetAccountByID)
-	router.PUT("/accounts/:id", ah.PutAccount)
-	router.DELETE("/accounts/:id", ah.DeleteAccount)
+	router.SetupRoutesV1(svr)
 
-	router.GET("/categories", ch.GetCategories)
-	router.POST("/categories", ch.PostCategory)
-	router.GET("/categories/:id", ch.GetCategoryByID)
-	router.PUT("/categories/:id", ch.PutCategory)
-	router.DELETE("/categories/:id", ch.DeleteCategory)
-
-	router.GET("/transactions", th.GetTransactions)
-	router.POST("/transactions", th.PostTransaction)
-	router.GET("/transactions/:id", th.GetTransactionByID)
-	router.PUT("/transactions/:id", th.PutTransaction)
-	router.DELETE("/transactions/:id", th.DeleteTransaction)
-
-	router.GET("cashflows/summary/:year/:month", cfh.GetCashflowSummary)
-	router.GET("cashflows/chart/:year/:month", cfh.GetCashflowChart)
-	router.GET("cashflows/card_balances/:year/:month", cfh.GetCashflowCardBalances)
-
-	router.Run("localhost:8080")
+	svr.Run("localhost:8080")
 }
---
+++ routes/routerV1.go
@@ -0,0 +1,53 @@
+package routes
+
+import (
+	h "rw_budget/api/handlers"
+	s "rw_budget/api/services"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutesV1(r *gin.Engine) {
+	setAccountRoutesV1(r)
+	setCashflowRoutesV1(r)
+	setCategoryRoutesV1(r)
+	setTransactionRoutesV1(r)
+}
+
+func setAccountRoutesV1(g *gin.Engine) {
+	ah := h.NewAccountHandler(s.NewAccountService())
+	ar := g.Group("/v1/accounts")
+	ar.GET("/", ah.GetAccounts)
+	ar.POST("/", ah.PostAccount)
+	ar.GET("/:id", ah.GetAccountByID)
+	ar.PUT("/:id", ah.PutAccount)
+	ar.DELETE("/:id", ah.DeleteAccount)
+}
+
+func setCashflowRoutesV1(g *gin.Engine) {
+	cfh := h.NewCashflowHandler(s.NewCashflowService())
+	cfr := g.Group("/v1/cashflows")
+	cfr.GET("cashflows/summary/:year/:month", cfh.GetCashflowSummary)
+	cfr.GET("cashflows/chart/:year/:month", cfh.GetCashflowChart)
+	cfr.GET("cashflows/card_balances/:year/:month", cfh.GetCashflowCardBalances)
+}
+
+func setCategoryRoutesV1(g *gin.Engine) {
+	ch := h.NewCategoryHandler(s.NewCategoryService())
+	cr := g.Group("/v1/categories")
+	cr.GET("/", ch.GetCategories)
+	cr.POST("/", ch.PostCategory)
+	cr.GET("/:id", ch.GetCategoryByID)
+	cr.PUT("/:id", ch.PutCategory)
+	cr.DELETE("/:id", ch.DeleteCategory)
+}
+
+func setTransactionRoutesV1(g *gin.Engine) {
+	th := h.NewTransactionHandler(s.NewTransactionService())
+	tr := g.Group("/v1/transactions")
+	tr.GET("/transactions", th.GetTransactions)
+	tr.POST("/transactions", th.PostTransaction)
+	tr.GET("/transactions/:id", th.GetTransactionByID)
+	tr.PUT("/transactions/:id", th.PutTransaction)
+	tr.DELETE("/transactions/:id", th.DeleteTransaction)
+}
